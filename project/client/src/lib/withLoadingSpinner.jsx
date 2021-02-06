4751c58253474b908747e2a5f8181085ef4b2a2a
Matthew Roll <mproll@protonmail.com>
2021-02-06 12:53:35 -0800

    bump version to 0.0.21

--- project/client/src/lib/withLoadingSpinner.jsx
+++ project/client/src/lib/withLoadingSpinner.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { CircularProgress } from '@material-ui/core'
 
 export function withLoadingSpinner(component, isLoading) {
-  console.log(component)
   return function () {
     return isLoading ? <CircularProgress /> : component
   }
