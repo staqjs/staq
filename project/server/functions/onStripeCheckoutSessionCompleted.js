778ec58be2a333bbe583f930050b1744295d4036
Matthew Roll <mproll@protonmail.com>
2021-02-14 13:58:12 -0800

    add onSuccess to checkout order fulfillment

--- project/server/functions/onStripeCheckoutSessionCompleted.js
+++ project/server/functions/onStripeCheckoutSessionCompleted.js
@@ -1,42 +1,44 @@
-import staqConfig from '../../staq'
-import { getSecret } from '../util'
+import staqConfig from "../../staq";
+import { getSecret } from "../util";
 
-const functions = require('firebase-functions')
-const _stripe = require("stripe")
+const functions = require("firebase-functions");
+const _stripe = require("stripe");
 
-let endpointSecret
-let fulfillOrder
+let endpointSecret;
+let fulfillOrder;
 
 async function onStripeCheckoutSessionCompleted(req, res, stripe) {
-    const payload = req.rawBody
-    const sig = req.headers['stripe-signature']
+  const payload = req.rawBody;
+  const sig = req.headers["stripe-signature"];
 
-    let event
+  let event;
 
-    try {
-        event = stripe.webhooks.constructEvent(payload, sig, endpointSecret)
-    } catch (err) {
-        console.error(err)
-        return res.status(400).send(`Webhook Error: ${err.message}`)
-    }
+  try {
+    event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+  } catch (err) {
+    console.error(err);
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
 
-    if (event.type === 'checkout.session.completed') {
-        const session = event.data.object
-        fulfillOrder(session)
-    }
-
-    res.status(200).end()
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object;
+    fulfillOrder(session, () => {
+      console.log("inside callback");
+      res.status(200).end();
+    });
+  } else {
+    res.status(200).end();
+  }
 }
 
-
 export default functions.https.onRequest(async (req, res) => {
-  const stripeSecretKey = await getSecret('stripe-secret-key')
-  const stripe = _stripe(stripeSecretKey)
+  const stripeSecretKey = await getSecret("stripe-secret-key");
+  const stripe = _stripe(stripeSecretKey);
 
-  const secretName = staqConfig.get('stripeCheckoutSessionCompletedSecretName')
-  endpointSecret = await getSecret(secretName)
+  const secretName = staqConfig.get("stripeCheckoutSessionCompletedSecretName");
+  endpointSecret = await getSecret(secretName);
 
-  fulfillOrder = staqConfig.get('stripeFulfillOrder')
+  fulfillOrder = staqConfig.get("stripeFulfillOrder");
 
-  onStripeCheckoutSessionCompleted(req, res, stripe)
-})
+  onStripeCheckoutSessionCompleted(req, res, stripe);
+});
