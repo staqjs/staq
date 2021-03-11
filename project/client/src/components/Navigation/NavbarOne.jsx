a91a7504b42f635a94a17b0846d0d39f12487135
Matthew Roll <mproll@protonmail.com>
2021-03-10 21:33:03 -0800

    add navbar menu for small screens

--- project/client/src/components/Navigation/NavbarOne.jsx
+++ project/client/src/components/Navigation/NavbarOne.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Select, ToolTipController } from 'react-tooltip-controller'
+import { FiMenu } from 'react-icons/fi'
 import { Link, useHistory } from 'react-router-dom'
 
 import staqConfig from '../../../../staq'
@@ -97,7 +99,29 @@ function NonAuthMenu(props) {
     handleClose()
   }
 
-  return <div className={''}></div>
+  return (
+    <div className={''}>
+      <ToolTipController detect="click" offsetY={10} offsetX={-50}>
+        <Select>
+          <button>
+            <FiMenu />
+          </button>
+        </Select>
+
+        <div
+          className={
+            'sjs-rounded-md sjs-border-2 sjs-border-gray-300 sjs-shadow-md sjs-p-2 sjs-flex sjs-flex-col'
+          }
+        >
+          {menuLinks.map((menuLink) => (
+            <Link key={menuLink.to} to={menuLink.to} className={'sjs-text-sm'}>
+              {menuLink.text}
+            </Link>
+          ))}
+        </div>
+      </ToolTipController>
+    </div>
+  )
 }
 
 function AuthMenu(props) {
@@ -120,7 +144,21 @@ function AuthMenu(props) {
     firebase.doSignOut()
   }
 
-  return <div className={''}></div>
+  return (
+    <div className={''}>
+      <ToolTipController>
+        <Select>
+          <button>
+            <FiMenu />
+          </button>
+        </Select>
+
+        <div>
+          <button onClick={onClickSignOut}>Sign Out</button>
+        </div>
+      </ToolTipController>
+    </div>
+  )
 }
 
 function SmallScreenavbar(props) {
@@ -135,27 +173,44 @@ function SmallScreenavbar(props) {
   )
 
   return (
-    <div className={''}>
-      <div className={''}>
-        <Link to="/" className={''}>
-          <span className={''}>
-            {logo && <img src={logo} className={''} />}
-            {siteTitle}
-          </span>
-        </Link>
+    <div
+      className={
+        'sjs-flex sjs-justify-between sjs-items-center sjs-px-6 sjs-py-2'
+      }
+    >
+      <Link to="/" className={''}>
+        <span className={''}>
+          {logo && <img src={logo} className={''} />}
+          <span className={'sjs-text-xl sjs-font-bold'}>{siteTitle}</span>
+        </span>
+      </Link>
 
-        {auth.currentUser ? (
-          <AuthMenu auth={auth} firebase={firebase} />
-        ) : (
-          <NonAuthMenu menuLinks={menuLinks} />
-        )}
-      </div>
+      {auth.currentUser ? (
+        <AuthMenu auth={auth} firebase={firebase} />
+      ) : (
+        <NonAuthMenu menuLinks={menuLinks} />
+      )}
     </div>
   )
 }
 
 function NavBarOne(props) {
-  return <RegularSizeNavbar {...props} />
+  const [smScreen, setSmScreen] = useState(window.innerWidth < 450)
+
+  const updateMedia = () => {
+    setSmScreen(window.innerWidth < 450)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', updateMedia)
+    return () => window.removeEventListener('resize', updateMedia)
+  })
+
+  return smScreen ? (
+    <SmallScreenavbar {...props} />
+  ) : (
+    <RegularSizeNavbar {...props} />
+  )
 }
 
 export default withFirebase(withAuth(NavBarOne))
